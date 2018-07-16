907aaf2e37e615333a76dcfb1deb75f9b1f8df1d
y0c <wonder789@naver.com>
2018-07-16 19:46:43 +0900

    wip(auth): auth check hoc

---
+++ memopad-frontend/src/components/hoc/PrivateRoute.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { connect, bindActionCreators } from 'react-redux';
+import * as user from 'store/modules/user';
+import storage from 'lib/storage';
+
+
+// export default PrivateRoute;
