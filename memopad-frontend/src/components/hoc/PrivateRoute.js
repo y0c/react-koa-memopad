769d13757aba8db599fc9b88d0868f3e59377828
y0c <wonder789@naver.com>
2018-07-17 02:10:41 +0900

    feat(hoc): add PrivateRoute hoc

--- memopad-frontend/src/components/hoc/PrivateRoute.js
+++ memopad-frontend/src/components/hoc/PrivateRoute.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { connect, bindActionCreators } from 'react-redux';
-import * as user from 'store/modules/user';
-import storage from 'lib/storage';
+import { Route, withRouter } from 'react-router-dom';
 
 
-// export default PrivateRoute;
+const PrivateRoute = ({component:Component,user, ...rest}) => {
+    let renderComponent = <Component/>;
+
+    if(!user.email) {
+        renderComponent = (
+            <div>로그인 후 이용하실 수 있습니다.</div>
+        )
+    }
+
+    return (
+        <Route
+            {...rest}
+            render={ () => renderComponent }
+        />
+    )
+}
+
+export default withRouter(PrivateRoute);
