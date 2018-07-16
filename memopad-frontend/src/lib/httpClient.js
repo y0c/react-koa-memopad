d05d212444ebc1d94751494e2a837357e717be25
y0c <wonder789@naver.com>
2018-07-17 02:59:12 +0900

    feat(http): add auth request interceptors

--- memopad-frontend/src/lib/httpClient.js
+++ memopad-frontend/src/lib/httpClient.js
@@ -3,9 +3,18 @@ import config from 'config';
 import storage from 'lib/storage';
 
 const instance = axios.create({
-    baseURL : config.backendUrl,
-    headers : { 'Authorization' : `Bearer ${storage.get('accessToken')}`}
+    baseURL : config.backendUrl
 });
 
+instance.interceptors.request.use(function(config) {
+    const token = storage.get('accessToken');
+
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, function(err) {
+    return Promise.reject(err);
+});
 
 export default instance;
