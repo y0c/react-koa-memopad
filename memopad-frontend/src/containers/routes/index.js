38d81a02f401cb34fe5262b959707dae450809d7
y0c <wonder789@naver.com>
2018-07-16 13:50:57 +0900

    feat(router): init default route page

--- memopad-frontend/src/containers/routes/index.js
+++ memopad-frontend/src/containers/routes/index.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
 import { Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
 import MainPage from './MainPage';
@@ -7,11 +8,11 @@ import MainPage from './MainPage';
 class Routes extends Component {
     render(){
         return (
-            <div>
+            <Container>
                 <Route exact path='/' component={MainPage} />
                 <Route path='/login' component={LoginPage} /> 
                 <Route path='/signup' component={SignupPage} /> 
-            </div>
+            </Container>
         )
     }
 }
