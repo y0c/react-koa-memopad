f66a57c9e33ea71b535a836a5f83142e578de330
y0c <wonder789@naver.com>
2018-07-17 17:33:58 +0900

    feat(signup): add signup action & reducer

--- memopad-frontend/src/lib/api/auth.js
+++ memopad-frontend/src/lib/api/auth.js
@@ -1,3 +1,4 @@
 import http from 'lib/httpClient';
 
 export const localLogin = form => http.post('/auth/login', form);
+export const localSignup = form => http.post('/auth/register', form);
--- memopad-frontend/src/store/modules/auth.js
+++ memopad-frontend/src/store/modules/auth.js
@@ -3,28 +3,45 @@ import { pender } from 'redux-pender';
 import { AuthApi } from 'lib/api';
 
 const LOCAL_LOGIN = 'auth/LOCAL_LOGIN';
+const LOCAL_SIGNUP = 'auth/LOCAL_SIGNUP';
 const FORM_CHANGE = 'auth/FORM_CHANGE';
+const SET_ERROR = 'auth/SET_ERROR';
+const INIT = 'auth/INIT';
 
 export const formChange = createAction(FORM_CHANGE);
 export const localLogin = createAction(LOCAL_LOGIN, AuthApi.localLogin);
+export const localSignup = createAction(LOCAL_SIGNUP, AuthApi.localSignup);
+export const setError = createAction(SET_ERROR);
+export const init = createAction(INIT);
 
 const initialState = {
     form : {
         email : '',
-        password : ''
+        password : '',
+        username : '',
+        passwordConfirm : '' 
     },
     error : '',
     loginResult : {}
 };
 
 export default handleActions({
+    [INIT]: (state, action) => {
+        return {
+            ...initialState
+        }
+    },
+    [SET_ERROR]: (state, action) => {
+        return {
+            ...state,
+            error: action.payload.error
+        }
+    },
     [FORM_CHANGE]:  (state, action ) => {
-        const { email, password } = action.payload;
         return {
             ...state,
             form : {
-               email,
-               password 
+                ...action.payload
             }
         }
     },
@@ -42,6 +59,15 @@ export default handleActions({
                 ...state,
                 error : action.payload.response.data.message
             }
+        }
+    }),
+    ...pender({
+        type : LOCAL_SIGNUP,
+        onFailure(state, action) {
+            return {
+                ...state,
+                error : action.payload.response.data.message
+            }    
         }
     })
 },initialState);
