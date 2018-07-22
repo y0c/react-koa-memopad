db2a8edafb1caae3adf6cfd1e550b0fc0ef4a46e
y0c <wonder789@naver.com>
2018-07-22 22:19:12 +0900

    fix(auth): authCheck middleware 'return' bug fix

--- memopad-backend/src/lib/middleware/authCheck.js
+++ memopad-backend/src/lib/middleware/authCheck.js
@@ -1,11 +1,11 @@
 export default () => 
     (ctx, next) => {
-        const { user } = ctx.request;
+    const { user } = ctx.request;
 
-        if (!user) {
-            ctx.status = 401;
-            return;
-        }
-        
-        next();
+    if (!user) {
+        ctx.status = 401;
+        return;
+    }
+    
+    return next();
 }
