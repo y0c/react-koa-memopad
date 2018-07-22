1173ebdcebed8a1dc6a6d5e3162fe1485a7e911b
y0c <wonder789@naver.com>
2018-07-23 01:50:35 +0900

    refactor(post): list item style change

--- memopad-frontend/src/components/MainPage/PostList.js
+++ memopad-frontend/src/components/MainPage/PostList.js
@@ -18,20 +18,28 @@ const HeaderLink = styled(({
     }
 `;
 
+const Item = styled(List.Item)`
+    &&&& {
+        margin-bottom: 10px;
+        padding-bottom: 10px;
+        border-bottom: 1px solid #eee;
+    }
+`;
+
 const PostList = ({
     posts
 }) => (
     <List>
         {
             posts.map( post => (
-                <List.Item key={post._id}>
+                <Item key={post._id}>
                     <List.Header as={HeaderLink} to={`/post/${post._id}`}>
                         {post.title}
                     </List.Header>
                     <List.Description>
                         {post.plainContent}
                     </List.Description>
-                </List.Item>
+                </Item>
             ))
         }
     </List>
